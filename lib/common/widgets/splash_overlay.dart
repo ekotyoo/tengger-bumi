2b9bd93422a0660ad08fd6734543f31e664a02df
JohanZERO <johanadi716@gmail.com>
2023-07-12 21:20:20 +0700

    update splash screen

--- lib/common/widgets/splash_overlay.dart
+++ lib/common/widgets/splash_overlay.dart
@@ -9,13 +9,33 @@ class SplashOverlay extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        body: Center(
-          child: CircleAvatar(
-            radius: SWSizes.s80,
-            backgroundColor: Theme.of(context).colorScheme.onPrimary,
-            child: Image.asset('assets/images/tetenger_bumi_logo.png'),
-          ),
+
+        // backgroundColor: Theme.of(context).primaryColor,
+        // body: Center(
+        //   child: CircleAvatar(
+        //     radius: SWSizes.s80,
+        //     backgroundColor: Theme.of(context).colorScheme.onPrimary,
+        //     child: Image.asset('assets/images/tetenger_bumi_logo.png'),
+        //   ),
+        // ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/tetenger_bumi_logo.png'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/filkom_logo.png'),
+                const SizedBox(width: 8),
+                Image.asset('assets/images/ub_logo.png'),
+                const SizedBox(width: 8),
+                Image.asset('assets/images/km_logo.png'),
+                const SizedBox(width: 8),
+                Image.asset('assets/images/mmd_ub_logo.png'),
+              ],
+            )
+          ],
         ),
       ),
     );
