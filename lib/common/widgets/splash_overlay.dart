420f7e95917cdfe6291ed81911a9d806d591ee3a
JohanZERO <johanadi716@gmail.com>
2023-07-14 22:47:22 +0700

    update splash screen

--- lib/common/widgets/splash_overlay.dart
+++ lib/common/widgets/splash_overlay.dart
@@ -22,7 +22,7 @@ class SplashOverlay extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset('assets/images/tetenger_bumi_logo.png'),
+            Expanded(child: Image.asset('assets/images/tetenger_bumi_logo.png')),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -33,8 +33,10 @@ class SplashOverlay extends StatelessWidget {
                 Image.asset('assets/images/km_logo.png'),
                 const SizedBox(width: 8),
                 Image.asset('assets/images/mmd_ub_logo.png'),
+
               ],
-            )
+            ),
+            const SizedBox(height: 16),
           ],
         ),
       ),
