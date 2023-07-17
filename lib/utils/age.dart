7f76707039e43a6e59aca3f3161ddd83a1012d7a
JohanZERO <johanadi716@gmail.com>
2023-07-17 12:17:18 +0700

    update detail screen

---
+++ lib/utils/age.dart
@@ -0,0 +1,14 @@
+
+String getAgeYearAndMonth(DateTime date){
+  DateTime now = DateTime.now();
+  int months = (now.year - date.year) * 12 + (now.month - date.month);
+  String age;
+  if(months > 12){
+    age = "${(months/12).toInt()} tahun ${months%12} bulan";
+  } else if (months > 0) {
+    age = "${months%12} bulan";
+  } else {
+    age = "0 bulan";
+  }
+  return age;
+}
